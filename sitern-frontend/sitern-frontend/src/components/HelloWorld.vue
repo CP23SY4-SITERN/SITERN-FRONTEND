fa1615513bcf78ce5d0190c0c304a491ad4c0ad0
jub <udomchai44@icloud.com>
2023-11-18 21:44:09 +0700

    add-homepage-interface

--- sitern-frontend/sitern-frontend/src/components/HelloWorld.vue
+++ sitern-frontend/sitern-frontend/src/components/HelloWorld.vue
@@ -9,28 +9,43 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css" rel="stylesheet">
+    <title>SITern</title>
+</head>
+<body class="bg-gray-100">
+    <header class="bg-blue-600 text-white p-4">
+        <h1 class="text-center">SITern</h1>
+    </header>
+    <nav class="bg-gray-200 p-4">
+        <ul class="flex space-x-4">
+            <li><a href="#" class="hover:text-blue-600">Home</a></li>
+            <li><a href="#" class="hover:text-blue-600">Intern Search</a></li>
+            <li><a href="#" class="hover:text-blue-600">Profile</a></li>
+            <li><a href="#" class="hover:text-blue-600">Latest Post</a></li>
+            <li><a href="#" class="hover:text-blue-600">Dashboard</a></li>
+            <li><a href="#" class="hover:text-blue-600">Find internship</a></li>
+            <li><a href="#" class="hover:text-blue-600">Login</a></li>
+            <li><a href="#" class="hover:text-blue-600">Register</a></li>
+        </ul>
+    </nav>
+    <main class="p-4">
+        <h2 class="text-2xl font-semibold mb-2">Status Dashboard</h2>
+        <div class="bg-white p-4 rounded-md shadow-sm">
+ <div class="relative">
+    <input type="text" placeholder="Search" class="bg-white text-gray-900 w-full py-2 px-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600">
+    <button class="absolute top-0 right-0 mt-2 mr-2 bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700">
+      Find Internship
+    </button>
+ </div>
+</div>
+    </main>
+    <footer class="bg-blue-600 text-white p-4">
+        <p>&copy; 2022 SITern. All rights reserved.</p>
+    </footer>
+</body>
 </template>
 
 <style scoped>

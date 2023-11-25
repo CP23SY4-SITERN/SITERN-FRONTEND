35e341f9da519002ae127ae8c43f603ccb3b9b6d
sSilence <sarin.kobfha@mail.kmutt.ac.th>
2023-11-26 00:41:06 +0700

    add icon, cookieService and add on internships

---
+++ sitern-frontend/src/pages/Internships.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container mx-auto">
+      <h1 class="text-3xl font-bold mb-4">Internships</h1>
+  
+      <ul class="flex flex-col gap-4">
+        <InternshipCard v-for="internship in internships" :key="internship.id" :internship="internship" />
+      </ul>
+    </div>
+  </template>
+  
+  <script setup>
+  import { defineComponent } from 'vue';
+  import { internshipsStore } from '../services/internships';
+  import InternshipCard from '../components/InternshipComponent/InternshipCard.vue';
+  
+  const internships = internshipsStore.internships;
+
+  </script>
---
+++ sitern-frontend/src/services/internships.js
@@ -0,0 +1,40 @@
+import { defineStore } from 'pinia';
+
+export const internshipsStore = defineStore('internships', () => {
+  const internships = ref([]);
+  const url = import.meta.env.VITE_API_URL;
+
+  const getAllJobs = async () => {
+    const res = await fetch(`${url}/users`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (res.status === 200) {
+      users.value = await res.json();
+    } else if (res.status === 401) {
+      let res = await res.json();
+      if (
+        res.message
+          .toLowerCase()
+          .match(
+            "please send refresh token to /refresh to refresh token".toLowerCase()
+          )
+      ) {
+        isLogin.refreshToken();
+      } else alert("please login");
+    } else if (res.status === 403) {
+      alert("You are not authorized to access this page.")
+    }else console.log("Error, cannot get data");
+  };
+  return {
+    getAllJobs,
+    internships
+  };
+});
+
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(internshipsStore, import.meta.hot));
+}
